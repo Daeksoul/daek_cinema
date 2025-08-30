89f6e3da8d773f5398f357be8d9185973f754f16
Daeksoul <adamdferguson.work@gmail.com>
2025-08-30 17:06:50 +0100

    Config.lua keybind addition
    Adding key bind for cinematic bars, for people that don't want to type the slash command - not yet fully implemented.

--- config.lua
+++ config.lua
@@ -6,3 +6,6 @@ Config.BarHeight = 130
 -- master volumes the cinematic audio cues
 Config.CinemaSoundInVolume  = 0.3
 Config.CinemaSoundOutVolume = 0.2
+
+-- set a usable keybind, for players that want to use one
+-- Config.CinemaKey = 57 -- F10 key
