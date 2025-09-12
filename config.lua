5fbaa1128f74de73a49d377c67d66d041bcf50f7
Daeksoul <129008230+Daeksoul@users.noreply.github.com>
2025-09-12 15:42:53 +0100

    Update config.lua
    Comment adjustments.

--- config.lua
+++ config.lua
@@ -3,9 +3,9 @@ Config = {}
 -- height of each bar in pixels
 Config.BarHeight = 130
 
--- master volumes the cinematic audio cues
+-- master volumes the cinematic audio cues - can be commented to disable/delete mp3s to disable.
 Config.CinemaSoundInVolume  = 0.3
 Config.CinemaSoundOutVolume = 0.2
 
--- set a usable keybind, for players that want to use one
+-- set a usable keybind, for players that want to use one (TBC if I feel like setting it up)
 -- Config.CinemaKey = 57 -- F10 key
