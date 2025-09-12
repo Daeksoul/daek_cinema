356ac8ed88e0a153cfcddc748b47461b3776df89
Daeksoul <129008230+Daeksoul@users.noreply.github.com>
2025-09-12 15:43:39 +0100

    Update client.lua
    Comment adjustments.

--- client.lua
+++ client.lua
@@ -6,8 +6,8 @@ SendNUIMessage({
     type = "cinema",
     enable = true,
     barHeight = Config.BarHeight,
-    soundInVolume  = Config.CinemaSoundInVolume,
-    soundOutVolume = Config.CinemaSoundOutVolume
+    soundInVolume  = Config.CinemaSoundInVolume,  --Can be commented out to disable/delete MP3s
+    soundOutVolume = Config.CinemaSoundOutVolume  --Can be commented out to disable/delete MP3s
 })
     SetNuiFocus(false, false)
 end, false)
